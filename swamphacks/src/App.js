8721de71331ea660a1379e9a3a2f8ec287f70d18
James <jamesgupchurch@knights.ucf.edu>
2019-01-19 11:09:48 -0500

    Refactored for style and spacing

--- swamphacks/src/App.js
+++ swamphacks/src/App.js
@@ -6,134 +6,139 @@ import firebase from './firebase.js';
 class App extends Component 
 {
 
-  constructor() 
-  {
+    constructor() {
+        super();
 
-    super();
+        this.state = {
+            currentItem: '',
+            username: '',
+            items: []
+        }
 
-    this.state = {
-      currentItem: '',
-      username: '',
-      items: []
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) 
-  {
-
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-
-  }
-
-  handleSubmit(e) 
-  {
-    e.preventDefault();
-
-    const itemsRef = firebase.database().ref('items');
-
-    const item = {
-      title: this.state.currentItem,
-      user: this.state.username
-    }
-
-    itemsRef.push(item);
-
-    this.setState({
-      currentItem: '',
-      username: ''
-    });
-  }
-
-  componentDidMount() 
-  {
-    const itemsRef = firebase.database().ref('items');
-
-    itemsRef.on('value', (snapshot) => {
-
-      let items = snapshot.val();
-      let newState = [];
-
-      for (let item in items) 
-      {
-        newState.push({
-          id: item,
-          title: items[item].title,
-          user: items[item].user
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
         });
-      }
+    }
 
-      this.setState({
-        items: newState
-      });
+    handleSubmit(e) {
+        e.preventDefault();
 
-    });
-  }
+        const itemsRef = firebase.database().ref('items');
 
-  removeItem(itemId) 
-  {
-    const itemRef = firebase.database().ref(`/items/${itemId}`);
-    itemRef.remove();
-  }
+        const item = {
+            title: this.state.currentItem,
+            user: this.state.username
+        }
 
-  render() 
-  {
-    return (
-      <div className='app'>
+        itemsRef.push(item);
 
-        <header>
+        this.setState({
+            currentItem: '',
+            username: ''
+        });
+    }
 
-            <div className="wrapper">
+    componentDidMount() {
+        const itemsRef = firebase.database().ref('items');
 
-              <h1>Fun Food Friends</h1>
-                             
+        itemsRef.on('value', (snapshot) => {
+
+            let items = snapshot.val();
+            let newState = [];
+
+            for (let item in items) {
+                newState.push({
+                    id: item,
+                    title: items[item].title,
+                    user: items[item].user
+                });
+            }
+
+            this.setState({
+                items: newState
+            });
+
+        });
+    }
+
+    removeItem(itemId) {
+        const itemRef = firebase.database().ref(`/items/${itemId}`);
+        itemRef.remove();
+    }
+
+    render() {
+        return (
+
+            <div className='app'>
+
+                <header>
+
+                    <div className="wrapper">
+
+                        <h1>SwampHacks</h1>
+                                 
+                    </div>
+
+                </header>
+
+                <div className='container'>
+
+                    <section className='add-item'>
+
+                        <form onSubmit={this.handleSubmit}>
+
+                            <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+                            <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+                            <button>Add Item</button>
+
+                        </form>
+
+                    </section>
+
+                    <section className='display-item'>
+
+                        <div className="wrapper">
+
+                            <ul>
+
+                                {this.state.items.map((item) => {
+
+                                    return (
+
+                                        <li key={item.id}>
+
+                                            <h3>{item.title}</h3>
+                                            <p>brought by: {item.user}
+
+                                                <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                                
+                                            </p>
+
+                                        </li>
+
+                                    )
+
+                                })}
+
+                            </ul>
+
+                        </div>
+
+                    </section>
+
+                </div>
+
             </div>
 
-        </header>
+        );
 
-        <div className='container'>
+    }
 
-          <section className='add-item'>
-
-                <form onSubmit={this.handleSubmit}>
-
-                  <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-                  <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-                  <button>Add Item</button>
-
-                </form>
-          </section>
-
-          <section className='display-item'>
-
-              <div className="wrapper">
-
-                <ul>
-                  {this.state.items.map((item) => {
-                    return (
-                      <li key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>brought by: {item.user}
-                          <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                        </p>
-                      </li>
-                    )
-                  })}
-                </ul>
-
-              </div>
-
-          </section>
-
-        </div>
-
-      </div>
-    );
-  }
 }
 
 export default App;
